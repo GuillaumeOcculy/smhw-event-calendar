dab0678a0b0378f728cefe1f9d4a527fff9fe5ca
Guillaume Occuly <occuly.guillaume@gmail.com>
2016-07-01 14:41:26 +0100

    User can create events and see them

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,21 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to root_path, notice: 'good'
+    else
+      redirect_to root_path, notice: 'not good'
+    end
+  end
+
+
+  private
+  def event_params
+    params.require(:event).permit(:starts_at, :ends_at, :description)
   end
 end
